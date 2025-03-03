6e3b256826656307f0da73dffcf6f92b73940162
Vnguyen2909 <nguyenvi290903@gmail.com>
2025-03-03 14:35:33 +0700

    'updatecode'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import MainLayout from "./components/Layout/Layout";
+import MyHeader from "./components/Header/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div>Ecommer</div>;
+  return (
+    <>
+      <MainLayout>
+        <MyHeader>Content</MyHeader>
+      </MainLayout>
+    </>
+  );
 }
 
 export default App;
